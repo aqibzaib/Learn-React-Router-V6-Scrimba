3f0b9ece47e8e09fc81dc61e3c9f18ab1d944895
Aqib <chaqib4u@gmail.com>
2024-05-01 20:23:34 +0500

    add styles

--- src/cmp/Host/HostVanPhoto.jsx
+++ src/cmp/Host/HostVanPhoto.jsx
@@ -2,5 +2,6 @@ import { useOutletContext } from "react-router-dom";
 
 export default function HostVanPhotos() {
   const { currentVan } = useOutletContext();
+  console.log("current van>>>", currentVan);
   return <img src={currentVan.imageUrl} className="host-van-detail-image" />;
 }
